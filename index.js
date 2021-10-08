ec79a1c6759007a1e80b88acbbe8a71b6d61b619
Tettie White <tettie_white@mail.ru>
2021-10-08 11:21:12 +0300

    IP-3: add json middleware

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
 async function start() {
     try {
         await mongoose.connect(process.env.ATLAS_URI);
 
-        app.get('/', (req, res) => {
-            res.send('Internship project');
-            console.log('Server is running...');
-        });
-
         app.listen(PORT);
+        console.log('Server is running...');
     } catch (error) {
         console.log(error);
     }
