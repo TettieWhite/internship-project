e2c48511a373ff7b3e50181e6be9fa48223dc425
Tettie White <tettie_white@mail.ru>
2021-10-21 13:12:49 +0300

    IP-6: add user authentication

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const router = Router();
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/User');
 const init = require('../models/init');
 
@@ -51,6 +53,79 @@ router.post('/', async (req, res) => {
                 error: "Password confirmation doesn't match password",
             });
         }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: error.message,
+        });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            res.status(401).send({
+                error: 'Auth failed',
+            });
+        }
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+            if (err) {
+                res.status(401).send({
+                    error: 'Auth failed',
+                });
+            }
+            if (result) {
+                const token = jwt.sign(
+                    {
+                        email: user.email,
+                        userId: user._id,
+                    },
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: 60 * 60,
+                    }
+                );
+                res.status(200).send({
+                    data: token,
+                    success: 'User has successfully logged in',
+                });
+            } else {
+                res.status(401).send({
+                    error: 'Auth failed',
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            error: error.message,
+        });
+    }
+});
+
+router.post('/me', async (req, res) => {
+    try {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+        if (!token) {
+            res.status(401).send({
+                error: 'User is not logged in',
+            });
+        }
+        jwt.verify(token, process.env.JWT_KEY, async (err, user) => {
+            if (err) {
+                res.status(403).send({
+                    error: 'Token is invalid',
+                });
+            }
+            const result = await User.findOne({ email: user.email });
+            res.status(200).send({
+                data: result,
+                success: 'Autorizated user info returned successfully',
+            });
+        });
     } catch (error) {
         console.log(error);
         res.status(500).send({
